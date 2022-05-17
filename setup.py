e92e5411eae17c9e2d18f0177a58306b146f06a2
Yijie Xia <yijiexia@pku.edu.cn>
2022-05-17 17:52:44 +0800

    update Xponge version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ f.close()
 #for formal
 setuptools.setup(
     name="Xponge",
-    version="1.2.5.12",
+    version="1.2.5.13",
     author="Yijie Xia",  
     author_email="yijiexia@pku.edu.cn", 
     description="A package for building molecular dynamics inputs for SPONGE",
