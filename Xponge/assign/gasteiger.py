7194bc4d645ed7b19f95b2bd549f872da6e576ee
pkuxyj <yijiexia@pku.edu.cn>
2022-10-09 22:31:23 +0800

    new charge model gasteiger

---
+++ Xponge/assign/gasteiger.py
@@ -0,0 +1,25 @@
+"""
+This **module** is used to calculate the Gasteiger charge.
+"""
+from rdkit.Chem import rdPartialCharges
+from ..helper import Xprint, set_global_alternative_names
+from ..helper.rdkit import assign_to_rdmol
+
+def gasteiger(assign):
+    """
+    This **function** is used to calculate the Gasteiger charge of an assignment
+
+    :param assign: the Assign instance
+    :return: a list of charges
+    """
+    rdmol = assign_to_rdmol(assign)
+    rdPartialCharges.ComputeGasteigerCharges(rdmol)
+    return [float(atom.GetProp("_GasteigerCharge")) for atom in rdmol.GetAtoms()]
+
+set_global_alternative_names()
+
+Xprint("""Reference for Gasteiger charge
+  Gasteiger, J., Marsili, M.
+    A new model for calculating atomic charges in molecules
+    Tetrahedron Letters, 1978 19(34) 3181-3184
+    DOI: 10.1016/S0040-4039(01)94977-9""")
